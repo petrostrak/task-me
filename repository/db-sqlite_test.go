f5fd55d6e739eeec0acf13fed7b658cae2face78
petrostrak <pit.trak@gmail.com>
2022-07-21 17:49:10 +0300

    Add TestSQLiteRepository_InsertTask

--- repository/db-sqlite_test.go
+++ repository/db-sqlite_test.go
@@ -1,11 +1,33 @@
 package repository
 
-import "testing"
+import (
+	"testing"
+	"time"
+)
 
 func TestSQLiteRepository_Migrate(t *testing.T) {
 	err := testRepo.Migrate()
 
 	if err != nil {
 		t.Error("migrate failed:", err)
+	}
+}
+
+func TestSQLiteRepository_InsertTask(t *testing.T) {
+	tsk := Task{
+		Title:       "title",
+		Description: "description",
+		Done:        true,
+		CreatedAt:   time.Now(),
+		CompletedAt: time.Now(),
+	}
+
+	result, err := testRepo.InsertTask(tsk)
+	if err != nil {
+		t.Error("insert failed:", err)
+	}
+
+	if result.ID <= 0 {
+		t.Error("invalid id sent back:", result.ID)
 	}
 }

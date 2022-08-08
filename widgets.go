fab5821f6aba3ffa5d4ff489d29293981970c240
petrostrak <pit.trak@gmail.com>
2022-08-08 18:12:30 +0300

    Remove unsused welcome message

--- widgets.go
+++ widgets.go
@@ -8,14 +8,6 @@ import (
 	"fyne.io/fyne/v2/canvas"
 )
 
-func (c *config) WelcomeMessage() *canvas.Text {
-	text := canvas.NewText("Welcome to taskMe!", color.White)
-	text.Alignment = fyne.TextAlignCenter
-	text.Resize(fyne.NewSize(600, 50))
-
-	return text
-}
-
 func (c *config) PendingTasks() *canvas.Text {
 	text := canvas.NewText(fmt.Sprintf("You have %d pending task(s)", c.CountPending()), color.White)
 	text.Alignment = fyne.TextAlignCenter
